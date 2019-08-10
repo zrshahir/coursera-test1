c9cea34c8b0244bca060169d2720f6bed4d39ab1
zrshahir <zrshahir@gmail.com>
2019-08-10 11:31:35 -0400

    Update script.js

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -20,11 +20,13 @@ Hello Larry
 Hello Paula
 Hello Laura
 Good Bye Jim
+
 WARNING!!! WARNING!!!
 The code does NOT currently work! It is YOUR job to make it work
 as described in the requirements and the steps in order to complete this
 assignment.
 WARNING!!! WARNING!!!
+
 */
 
 // STEP 1: (NOTHING TO DO. ALREADY DONE FOR YOU)
